7238fe511a671194e11faa368410954065938303
vin0veritas <127625670+vin0veritas@users.noreply.github.com>
2024-12-16 21:53:42 +0300

    add Widget with redisign

---
+++ app/src/main/java/com/example/clothesapp/Activity/MainWidgetProvider.kt
@@ -0,0 +1,46 @@
+package com.example.clothesapp.Widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.RemoteViews
+import com.example.clothesapp.Activity.CartActivity
+import com.example.clothesapp.Activity.MainActivity
+import com.example.clothesapp.R
+
+class MainWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        for (appWidgetId in appWidgetIds) {
+            val views = RemoteViews(context.packageName, R.layout.widget_main)
+
+
+            val cartIntent = Intent(context, CartActivity::class.java)
+            val cartPendingIntent = PendingIntent.getActivity(context, 0, cartIntent, PendingIntent.FLAG_IMMUTABLE)
+            views.setOnClickPendingIntent(R.id.widget_cart_button, cartPendingIntent)
+
+
+            val popularIntent = Intent(context, MainActivity::class.java)
+            val popularPendingIntent = PendingIntent.getActivity(context, 1, popularIntent, PendingIntent.FLAG_IMMUTABLE)
+            views.setOnClickPendingIntent(R.id.widget_popular_button, popularPendingIntent)
+
+
+            val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=Google+Plex+Mountain+View+CA"))
+            mapIntent.setPackage("com.google.android.apps.maps")
+            val mapPendingIntent = PendingIntent.getActivity(context, 2, mapIntent, PendingIntent.FLAG_IMMUTABLE)
+            views.setOnClickPendingIntent(R.id.widget_map_button, mapPendingIntent)
+
+
+            val balanceIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://lime-shop.digift.ru"))
+            val balancePendingIntent = PendingIntent.getActivity(context, 3, balanceIntent, PendingIntent.FLAG_IMMUTABLE)
+            views.setOnClickPendingIntent(R.id.widget_balance_button, balancePendingIntent)
+
+
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
